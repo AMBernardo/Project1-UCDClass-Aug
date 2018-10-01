181d55fdd8dc6d088dac296d3288f4362ae3220b
troy <gordontaaj@gmail.com>
2018-10-01 15:43:39 -0700

    Bridge api call working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,3 +3,22 @@ $('.dropdown-trigger').dropdown();
 $('.sidenav').sidenav();
 
 });
+
+
+
+
+ $.ajax({
+    url: ' https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1',
+    type: "GET", /* or type:"GET" or type:"PUT" */
+    dataType: "json",
+    data: {
+    },
+    success: function (result) {
+        console.log(result);    
+    },
+    error: function () {
+        console.log("error");
+    }
+});
+
+
