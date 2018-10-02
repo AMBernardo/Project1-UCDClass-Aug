99dd8945dedc4f2c5a820429f129306344f0e978
troy <gordontaaj@gmail.com>
2018-10-02 16:26:27 -0700

    Map functionality complate so far awaiting larger dataset

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,62 @@
 jQuery(document).ready(function(){
-    $('.dropdown-trigger').dropdown();
-    $('.sidenav').sidenav();
-    
+$('.dropdown-trigger').dropdown();
+$('.sidenav').sidenav();
+
+});
+
+
+
+function getDataBridge(){
+    var URL =  ' https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1' 
+    URL += '?' + $.param({
+        'near': $('#locationname').val().trim(),
+        
     });
-    
-    $('#submit').on('click', function(event) {
-        event.preventDefault()
-       
-        var URL =  ' https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1&limit=9' 
-        URL += '&' + $.param({
-            'near': $('#city').val(),
-            
+    $.ajax({
+        url: queryURL,
+        type: "GET", /* or type:"GET" or type:"PUT" */
+        dataType: "json",
+        data: {
+        },
+        success: function (result) {
+            console.log(result);    
+        },
+        error: function () {
+            console.log("error");
+        }
+    });
+};
+
+
+$.ajax({
+    type: "GET",
+    url: "https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1",
+    data: "",
+    success: function(results) {
+        for (var i = 0; i < results.bundle.length; i++) {
+            var coords = results.bundle[i].Coordinates;
+            var latLng = new google.maps.LatLng(coords[1],coords[0]);
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map
         });
+    }
+}});
+
+var map;
+function initMap(){
+map = new google.maps.Map(document.getElementById('map'), {
+center: new google.maps.LatLng(37.773972,-122.431297),
+zoom: 11
+
+})}
+
+
+      // Loop through the results array and place a marker for each
+      // set of coordinates.
+      window.eqfeed_callback = function(result) {
         $.ajax({
-            url: URL,
+            url:  ' https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1',
             type: "GET", /* or type:"GET" or type:"PUT" */
             dataType: "json",
             data: {
@@ -25,43 +68,16 @@ jQuery(document).ready(function(){
                 console.log("error");
             }
         });
-    });
-    
-
-   
-    var map;
-function initMap()
-{
-map = new google.maps.Map(document.getElementById('map'), {
-center: new google.maps.LatLng(37.773972,-122.431297),
-zoom: 11
-});
- 
-  var script = document.createElement('script');
-  script.src ='https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1' ;
-  document.getElementsByTagName('head')[0].appendChild(script);
-      }
-
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      window.eqfeed_callback = function(result) {
         for (var i = 0; i < result.bundle.length; i++) {
-          var Latitude = result.bundle[i].latitude;
-          var Longitude = result.bundle[i].longitude;
-          var latLng = Latitude.add(Longitude);
+          var coords = result.bundle[i].Coordinates;
+        var latLng = new google.maps.LatLng(coords[0],coords[1]);
           var marker = new google.maps.Marker({
             position: latLng,
-            map: map
+            map: map,
+            marker: marker
           });
         }
       }
-
-
-
-
-
-
-
 
 function newLocation(newLat,newLng)
 {
@@ -88,5 +104,3 @@ $("#3").on('click', function ()
 newLocation(30.267153, -97.7430608);
 });
 });
-
-
