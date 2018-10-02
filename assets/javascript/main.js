166725ce04dfee26120d7bfb33352144724fea40
TroyG <gordontaaj@gmail.com>
2018-10-01 21:15:57 -0700

    local user listing data working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,29 @@
 jQuery(document).ready(function(){
-$('.dropdown-trigger').dropdown();
-$('.sidenav').sidenav();
-
-});
-var dataID = 
- $.ajax({
-    url: 'https://rets.io/api/v2/abor_ref/listings?access_token=520a691140619b70d86de598796f13c1&near=Austin',
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
     
-    type: "GET", /* or type:"GET" or type:"PUT" */
-    dataType: "json",
-    data: {
-    },
-    success: function (result) {
-        console.log(result);    
-    },
-    error: function () {
-        console.log("error");
-    }
-});
-
-
+    });
+    
+    $('#submit').on('click', function(event) {
+        event.preventDefault()
+       
+        var URL =  ' https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1&limit=9' 
+        URL += '&' + $.param({
+            'near': $('#city').val(),
+            
+        });
+        $.ajax({
+            url: URL,
+            type: "GET", /* or type:"GET" or type:"PUT" */
+            dataType: "json",
+            data: {
+            },
+            success: function (result) {
+                console.log(result)  
+            },
+            error: function () {
+                console.log("error");
+            }
+        });
+    });
+    
