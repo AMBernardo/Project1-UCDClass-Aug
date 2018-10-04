28e40000518be2848b2e2a74597547940d3cddf7
JecoLantern <jbayonke@hotmail.com>
2018-10-03 19:57:38 -0700

    Privacy policy and links

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function(){
     $('.dropdown-trigger').dropdown();
     $(".sidenav").sidenav();
     $(".tabs").tabs();
+    $(".modal").modal();
+    $(".parallax").parallax();
 
 
     // $(".sidenav").on("click", function(){
