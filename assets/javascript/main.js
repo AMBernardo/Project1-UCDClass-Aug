4ae1ce2a3b92e3172d912b9b29ce229693accbe1
Retr0 <Ghozt24@gmail.com>
2018-10-03 19:50:33 -0700

    organized links

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,5 +2,13 @@ jQuery(document).ready(function(){
 $('.dropdown-trigger').dropdown();
 $(".sidenav").sidenav();
 $(".tabs").tabs();
+$('.parallax').parallax();
+$('.carousel').carousel();
+$('.slider').slider({full_width: true});
+$('.carousel-slider').slider({full_width: true});
 
 });
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+    });
