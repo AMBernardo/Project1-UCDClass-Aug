87568fb6b6e33f4b0f1c05cbe520a695c78622a8
Austin.Shepherd <Austin.Shepherd.96@gmail.com>
2018-10-03 18:44:14 -0700

    result page v3

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,7 +5,7 @@ $('.sidenav').sidenav();
 });
 
 
-
+//Text Search Ajax Call
 function getDataBridge(){
     var URL = ' https://rets.io/api/v2/test/listings?access_token=520a691140619b70d86de598796f13c1' 
     URL += '&' + $.param({
@@ -37,11 +37,29 @@ function getDataBridge(){
 function openResults(){
     window.open('results.html','_self');
 }
-function changeBlade(){
-    //function that will change the display in the display blade
-    //write all the info in 3 sepperate lists and set all to 'display:none'
+
+$('div.card').on("click", function(event){
     //on click if postPoint>child.attr('display'==none) set child.display = inline & set otherchildren.display = none
-}
+    //need to denote what certain clicks do.
+    //right now clicking on the picture opens the blade but i think that should just link to the reletive houses info page
+    //its is making it hard to set the click event that changes the blade content. talk to ux about options there.
+});
+//Functions for displaying arrays 
+    //ex. el.text(arrayDisplay(arry))
+function arrayDisplay(arry){
+    for(var i = 0; i< arry.length; i++){
+        return arry[i]; 
+    };
+};
+function booleanArrayDisplay(bln, arry){
+    if(bln === true){
+        for(var i = 0; i< arry.length; i++){
+            return arry[i]; 
+        };
+    }else return 'N/A';
+};
+
+
 function generateCards(data){
     var a = $('div#rowPost');
     for(var i = 0; i < data.bundle.length; i++){
@@ -50,32 +68,34 @@ function generateCards(data){
         if(result.Media[0]) var imgurl = result.Media[0].MediaURL;
         else var imgurl = './assets/images/image.png';
         
-     
+    //card generation 
     a.append(
         $('<div/>', {'class': 'col s12 m4'}).append(
             $('<div/>', {'class': 'card blue-grey darken-1'}).append(
                 $('<div/>', {'class': 'card-content white-text'}).append(
                     $('<span/>', {'class': 'card-title cardTitle'}).text('$'+result.ListPrice)
                 )
-            ).append(
+            ).append(//Price Header 
                 $('<div/>', {'class': 'row'}).append(
                     $('<div/>',{'class': 'col s10 offset-s1'}).append(
                         $('<div/>',{'class':'card'}).append(
                             $('<div/>',{'class':'card-image waves-effect waves-block waves-light'}).append(
-                                $('<img>', {'class':'responsive-img activator'}).attr('src',imgurl).attr('alt','test pic')
+                            //image block=================
+                                $('<img>', {'class':'responsive-img'}).attr('src',imgurl).attr('alt','test pic')
                             )
+                    //data catagories
                         ).append(
-                            $('<div/>',{'class':'card-content'}).append(
+                            $('<div/>',{'class':'card-content bladeCallParent'}).append(
                                 $('<span/>',{'class':'card-title activator grey-text text-darken-4'}).text('Housing Data').append(
-                                    $('<i/>', {'class':'material-icons right'}).text('more_vert').attr('id', 'hData')
+                                    $('<i/>', {'class':'material-icons right bladeCall'}).text('more_vert').attr('data-display', 'true')
                                 )
                             ).append(
                                 $('<span/>',{'class':'card-title activator grey-text text-darken-4'}).text('Agent Data').append(
-                                    $('<i/>', {'class':'material-icons right'}).text('more_vert').attr('id', 'aData')
+                                    $('<i/>', {'class':'material-icons right bladeCall'}).text('more_vert').attr('data-display', 'false')
                                 )
                             ).append(
                                 $('<span/>',{'class':'card-title activator grey-text text-darken-4'}).text('Lot Data').append(
-                                    $('<i/>', {'class':'material-icons right'}).text('more_vert').attr('id', 'lData')
+                                    $('<i/>', {'class':'material-icons right bladeCall'}).text('more_vert').attr('data-display', 'false')
                                 )
                             ).append(
                                 $('<p/>').append(
@@ -85,6 +105,7 @@ function generateCards(data){
                                 )
                             )
                         ).append(
+                        //card blades========================
                             $('<div/>', {'class':'card-reveal'}).append(
                                 $('<span/>',{'class':'card-title grey-text text-darken-4'}).append(
                                 $('<i/>',{'class':'material-icons right'}).text('close')
@@ -95,7 +116,7 @@ function generateCards(data){
                                 ).append(
                                     $('<span/><br>',{'class':'lotData'}).attr('style', 'display:none').text('Lot Info')
                                 )
-                            ).append(
+                            ).append(// Housing Data====
                                 $('<p/>',{'class':'postPoint'}).append(
                                     $('<ul/>',{'class':'housingData'}).attr('style', 'display: inline').text(result.UnparsedAddress).append(
                                         $('<li/>').text(' Beds: ' + result.BedroomsTotal)
@@ -104,9 +125,11 @@ function generateCards(data){
                                     ).append(
                                         $('<li/>').text(' Half Baths: ' + result.BathroomsHalf)
                                     ).append(
-                                        $('<li/>').text(' ')
-                                    )//appliances, utilities, 
-                                ).append(
+                                        $('<li/>').text('Heating Options: ' + booleanArrayDisplay(result.HeatingYN, result.Heating))
+                                    ).append(
+                                        $('<li/>').text('Laundry Features: ' + arrayDisplay(result.LaundryFeatures))
+                                    )// add appliances, utilities, 
+                                ).append(//Agent Data====
                                     $('<ul/>',{'class':'agentData'}).attr('style','display: none').text(result.ListAgentFullName).append(
                                         $('<li/>').text('Contact Number: ' + result.ListAgentPreferredPhone)
                                     ).append(
@@ -116,14 +139,14 @@ function generateCards(data){
                                     ).append(
                                         $('<li/>').text('Listing Office: ' + result.ListOfficeName)
                                     )
-                                ).append(
+                                ).append(//Lot Data====
                                     $('<ul/>',{'class':'lotData'}).attr('style','display:none').append(
                                         $('<li/>').text('Lot Size: ' + result.LotSizeSquareFeet + ' SqFt.')
                                     ).append(
                                         $('<li/>').text('Living Area: ' + result.LivingArea + ' SqFt.' )
                                     ).append(
                                         $('<li/>').text('Exterior Features: ' + result.ExteriorFeatures)
-                                    )//
+                                    )
                                 )
                             )
                         )
@@ -132,6 +155,6 @@ function generateCards(data){
             )
         )
     );
-                            }//loop close
+                            }//loop close===========
 };
 
